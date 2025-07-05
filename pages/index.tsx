158cec95315d29f04b3800483a058085a1021ebf
manoj00026 <manojprasad26@outlook.com>
2025-07-05 18:17:32 +0530

    Fix: use <Link> instead of <a> for internal navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,6 +27,15 @@ export default function Home() {
             </span>
           </Link>
         </div>
+
+        {/* Example of another navigation fixed correctly */}
+        <div className="flex justify-center mt-6">
+          <Link href="/pricing/">
+            <span className="inline-block text-blue-600 hover:underline text-base">
+              Go to Pricing Page
+            </span>
+          </Link>
+        </div>
       </main>
 
       <footer className="text-center mt-12 text-sm text-gray-600">
